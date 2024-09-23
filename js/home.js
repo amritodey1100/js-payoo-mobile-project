8cf2bee978feac6462038a9b3c0eba8586782211
Amrito Dey <amritodey07@gmail.com>
2024-09-23 19:44:08 +0600

    added some js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home added')
+// console.log('home added')
+
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const addMoneyinput = document.getElementById('input-add-money').value;
+    
+    const pinNumber = document.getElementById('input-pin-number').value;
+    console.log(pinNumber)
+
+  });
